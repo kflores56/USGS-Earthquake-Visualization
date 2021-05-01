eb10c8d020af30e928bcd3475ec66c0726c68731
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-05-01 16:41:44 -0500

    update code to pull earthquake data, not producing map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,88 +1,89 @@
 //// CREATE LOCATION MARKERS ////
 
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// function to determine marker color (magnitude) 
-// function chooseColor()
+// // function to determine marker color (magnitude) 
+// // function chooseColor()
 
-// function to determine marker size (depth)
+// // function to determine marker size (depth)
 
 // grab geoJSON data & bind pop-up information
 
-// define array
-var eqMarkers = [];
-var coordinates = []
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
 
-// loop through locations to create marker
-for (var i = 0; i < link.length; i++) {
-  eqMarkers.push(
-    L.circle([link.features.geometry.coordinates[0], features.geometry.coordinates[1]], {
-      stroke: false, 
-      fillOpacity: 0.50, 
-      // color: function
-      // fillColor: function
-      radius: features.geometry.coordinates[2]
-    })
-    .bindPopup("<h3>Location: " + features.properties.place + "<h3><h3>Magnitude: " + features.properties.mag + "</h3>")
-    )
-}
-// var magnitude = features.properties.mag
-// var time = features.properties.time
-// var lat = features.geometry.coordinates[0]
-// var long = features.geometry.coordinates[1]
+function createFeatures(earthquakeData) {
   
-//// CREATE BASE MAPS ////
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>Location: " + feature.properties.place + 
+      "</h3><h3>Magnitude: " + feature.properties.mag + "</h3>");
+  }
 
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  createMap(earthquakes);
+}
+
+//// CREATE MAP FUNCTION ////
+
+function createMap(earthquakes) {
+
+  // defining basemap layers
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "satellite-v9",
     accessToken: API_KEY
-  })
-
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  });
+  
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
   });
 
-var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "satellite-v9",
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
   });
 
-//// CREATE LAYER GROUPS ////
+  //// CREATE LAYER GROUPS ////
 
-// overlay object
-var overlayMaps = {
-  "Earthquakes" : eqMarkers
-}
-
-// baseMaps object
-var baseMaps = {
-    "Street Map": streetmap,
+  // baseMaps object
+  var baseMaps = {
+    "Satellite Map": satellitemap,
     "Light Map": lightmap,
-    "Satellite Map": satellitemap
+    "Street Map": streetmap,
   };
 
-//// CREATE MAP ////
+  // overlay object
+  var overlayMaps = {
+    Earthquakes : earthquakes
+  };
 
-// Define a map object
-var myMap = L.map("map", {
-  center: [0, 0],
-  zoom: 3, 
-  layers: [satellitemap]
-});
+  //// CREATE MAP ////
 
-// Pass map layers into layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, {
-  collapsed: false
-}).addTo(myMap);
+  // Define a map object
+  var myMap = L.map("mapid", {
+    center: [0, 0],
+    zoom: 3, 
+    layers: [satellitemap, earthquakes]
+  });
 
+  // Pass map layers into layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
 
